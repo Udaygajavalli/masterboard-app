88394b206a71c109062d9674e5fa7f644ddc8801
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-05-04 20:03:58 +0530

    added prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDBt3UQVUyLzuIvtH6iiGW9qlv2V4OSxvI',
+    authDomain: 'masterboard-53af4.firebaseapp.com',
+    projectId: 'masterboard-53af4',
+    storageBucket: 'masterboard-53af4.appspot.com',
+    messagingSenderId: '720312005277',
+    appId: '1:720312005277:web:b9799e8159511f063d5a74',
+    measurementId: 'G-JHT3E30J3C',
+  },
+  googleAPI_KEY: 'AIzaSyD3N9WUBg6QEH7aFvLEsDwoiOS3BwniL3c',
 };
